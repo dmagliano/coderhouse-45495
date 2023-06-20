3a0623a59871190d2c686c989cc08c8a7117e20f
dmagliano <diogo.magliano@gmail.com>
2023-06-20 19:27:46 -0300

    Aula 10 - desafio final

---
+++ Aula10/websocket-server/src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import viewsRouter from './routes/views.router.js';
+import {Server} from 'socket.io';
+
+const app = express();
+const server = app.listen(8080, () => console.log('Servidor rodando na porta 8080'));
+
+const socketServer = new Server(server);
+
+const viewFolder = 'views';
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', viewFolder);
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+app.use('/', viewsRouter);
+
+socketServer.on('connection', socket => {
+    console.log('Novo cliente conectado ao servidor!');
+
+    socket.on('message', data => {
+        console.log(`Mensagem recebida de ${data}`);
+
+        const socketId = socket.id;
+
+        socketServer.emit('message', 'Servidor recebeu: ' + data + ' do cliente ' + socketId);
+    });
+});
