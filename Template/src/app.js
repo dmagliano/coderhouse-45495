f5756c6b4a1470c6c8f54e3fdf4fa4b763b66c85
dmagliano <diogo.magliano@gmail.com>
2023-06-22 19:01:41 -0300

    Template para projetos com express, handlebars e socketio

---
+++ Template/src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import __dirname from './utils.js';
+import handlebars from 'express-handlebars';
+import viewsRouter from './routes/views.router.js';
+import { Server } from 'socket.io';
+
+const serverPort = 8080;
+const app = express();
+const server = app.listen(serverPort, () => console.log(`Servidor rodando na porta ${serverPort}`));
+
+const io = new Server(server);
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+app.use('/', viewsRouter);
