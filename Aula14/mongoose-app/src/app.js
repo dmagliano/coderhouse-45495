5222a36ae68d701a4a66242abc1e5b77b70eebc7
dmagliano <diogo.magliano@gmail.com>
2023-07-04 08:52:00 -0300

    Fixing secret leak

--- Aula14/mongoose-app/src/app.js
+++ Aula14/mongoose-app/src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const port = 8080;
 const server = app.listen(port, () => console.log(`Server listening on port ${port}`));
 
-const mongoAtlasUrl = 'mongodb+srv://diogomagliano:VqJ8vbXbJaqjat8F@cluster0.ppgtl4o.mongodb.net/?retryWrites=true&w=majority'
+const mongoAtlasUrl = 'mongodb+srv://diogomagliano:<password>@cluster0.ppgtl4o.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.connect(mongoAtlasUrl)
 .catch((error) => {
