b74620ce0ad4d405a5fc23ed23e4511b428e42c0
dmagliano <diogo.magliano@gmail.com>
2023-07-25 21:04:10 -0300

    Aula 20 recovery de senha

--- Aula19/login-app/src/routes/login.router.js
+++ Aula19/login-app/src/routes/login.router.js
@@ -1,7 +1,8 @@
-import {Router} from 'express';
-import {UserModel} from '../models/user.model.js';
+import { Router } from 'express';
+import { UserModel } from '../models/user.model.js';
 import cookieParser from 'cookie-parser';
 import session from "express-session";
+import { createHash } from '../utils.js';
 import { isValidPassword } from '../utils.js';
 
 const router = Router();
@@ -15,22 +16,39 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) => {
-    let {email, password} = req.body;
+router.post('/recovery', async (req, res) => {
+    let { email, password } = req.body;
     try {
-        const user = await UserModel.findOne({email: email});
-        if(user == null) {
+        const user = await UserModel.findOne({ email: email });
+        if (user == null) {
+            res.status(404)
+        } else {
+            user.password = createHash(password);
+            user.save()
+            res.send('Senha alterada com sucesso');
+        }
+    }
+    catch (error) {
+        res.status(400).json({ erro: error.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    let { email, password } = req.body;
+    try {
+        const user = await UserModel.findOne({ email: email });
+        if (user == null) {
             res.status(404)
         }
-        if(!isValidPassword(user, password)) {
-            res.status(403).json({erro: "Senha incorreta"});
+        if (!isValidPassword(user, password)) {
+            res.status(403).json({ erro: "Senha incorreta" });
         } else {
-        req.session.user = email;
-        req.session.logged = true;
-        res.send('Login realizado com sucesso');
+            req.session.user = email;
+            req.session.logged = true;
+            res.send('Login realizado com sucesso');
         }
     } catch (error) {
-        res.status(400).json({erro: error.message});
+        res.status(400).json({ erro: error.message });
     }
 });
 
